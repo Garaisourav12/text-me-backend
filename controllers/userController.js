e4f6e04653cd94a7820b5348ce925f0ce462670c
Garaisourav12 <garaisourav12@gmail.com>
2024-08-22 22:13:47 +0530

    sameSite lax

--- controllers/userController.js
+++ controllers/userController.js
@@ -86,7 +86,7 @@ module.exports.login = async (req, res) => {
             .cookie("token", token, {
                 maxAge: 1000 * 60 * 60 * 24,
                 httpOnly: true,
-                sameSite: "strict",
+                sameSite: "lax",
             })
             .json({
                 message: "Logged in successfully",
