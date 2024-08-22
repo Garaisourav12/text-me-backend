6f29dd862c74ae5ed78e5632536f0e2ce5364632
Garaisourav12 <garaisourav12@gmail.com>
2024-08-22 15:37:31 +0530

    Revert "Text me backend"
    This reverts commit 0a4e85c18cba99f7d9bd25a18fc0e41eccda2529.

--- socket/index.js
+++ socket/index.js
@@ -7,8 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["https://text-me-client.vercel.app", "http://localhost:3000"],
-        methodes: ["GET", "POST", "PUT", "DELETE"],
+        origin: ["https://text-me-seven.vercel.app", "http://localhost:3000"],
         methods: ["GET", "POST"],
     },
 });
