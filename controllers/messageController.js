c43a98c28e6a4d8b32520e29fce282b7711b98fb
Garaisourav12 <garaisourav12@gmail.com>
2024-09-13 15:41:02 +0530

    Revert "Send senderId with message through ws"
    This reverts commit 525529b828860f01a2155157a2cd6eabfa9850b7.

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,85 +3,82 @@ const Conversation = require("../models/conversationModel");
 const { io, getSocketId } = require("../socket/index");
 
 module.exports.sendMessage = async (req, res) => {
-	const senderId = req.userId;
-	const receiverId = req.params.id;
-	const message = req.body.message;
+    const senderId = req.userId;
+    const receiverId = req.params.id;
+    const message = req.body.message;
 
-	if (!message) {
-		return res.status(400).json({ error: "You can't send empty message" });
-	}
+    if (!message) {
+        return res.status(400).json({ error: "You can't send empty message" });
+    }
 
-	if (!receiverId) {
-		return res.status(400).json({ error: "Receiver id is missing" });
-	}
+    if (!receiverId) {
+        return res.status(400).json({ error: "Receiver id is missing" });
+    }
 
-	// As of now I will not allow user to send message to himself
-	if (senderId === receiverId) {
-		return res
-			.status(400)
-			.json({ error: "You can't send message to yourself" });
-	}
+    // As of now I will not allow user to send message to himself
+    if (senderId === receiverId) {
+        return res
+            .status(400)
+            .json({ error: "You can't send message to yourself" });
+    }
 
-	try {
-		let gotConversation = await Conversation.findOne({
-			participentsId: {
-				$all: [senderId, receiverId],
-			},
-		});
+    try {
+        let gotConversation = await Conversation.findOne({
+            participentsId: {
+                $all: [senderId, receiverId],
+            },
+        });
 
-		if (!gotConversation) {
-			gotConversation = new Conversation({
-				// create new conversation
-				participentsId: [senderId, receiverId],
-			});
-		}
+        if (!gotConversation) {
+            gotConversation = new Conversation({
+                // create new conversation
+                participentsId: [senderId, receiverId],
+            });
+        }
 
-		// We will not save this message in database directly
-		const newMessage = new Message({
-			senderId,
-			receiverId,
-			message,
-		});
+        // We will not save this message in database directly
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message,
+        });
 
-		await newMessage.save();
+        await newMessage.save();
 
-		gotConversation.messages.push(newMessage);
-		await gotConversation.save();
+        gotConversation.messages.push(newMessage);
+        await gotConversation.save();
 
-		// Scocket.io
-		const receiverSocketId = getSocketId(receiverId);
-		if (receiverSocketId) {
-			io.to(receiverSocketId).emit("newMessage", {
-				senderId,
-				message: newMessage,
-			});
-		}
+        // Scocket.io
+        const receiverSocketId = getSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
-		return res.status(200).json({ message: newMessage });
-	} catch (error) {
-		return res.status(500).json({ error: error.message });
-	}
+        return res.status(200).json({ message: newMessage });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 };
 
 module.exports.getMessages = async (req, res) => {
-	const senderId = req.userId;
-	const receiverId = req.params.id;
+    const senderId = req.userId;
+    const receiverId = req.params.id;
 
-	try {
-		let conversation = await Conversation.findOne({
-			participentsId: {
-				$all: [senderId, receiverId],
-			},
-		});
+    try {
+        let conversation = await Conversation.findOne({
+            participentsId: {
+                $all: [senderId, receiverId],
+            },
+        });
 
-		if (!conversation) {
-			return res.status(200).json({ messages: [] });
-		}
+        if (!conversation) {
+            return res.status(200).json({ messages: [] });
+        }
 
-		const messages = await conversation.populate("messages");
+        const messages = await conversation.populate("messages");
 
-		return res.status(200).json({ messages: messages.messages });
-	} catch (error) {
-		return res.status(500).json({ error: error.message });
-	}
+        return res.status(200).json({ messages: messages.messages });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 };
