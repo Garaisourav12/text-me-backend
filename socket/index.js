68136bcb0692af55230af638dfccfb4980e4be24
Garaisourav12 <garaisourav12@gmail.com>
2024-09-26 00:28:50 +0530

    Improvement in socket

--- socket/index.js
+++ socket/index.js
@@ -6,10 +6,10 @@ const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: true,
-        methods: ["GET", "POST"],
-    },
+	cors: {
+		origin: true,
+		methods: ["GET", "POST"],
+	},
 });
 
 const userSocketMap = {}; // {userId->socketId}
@@ -17,17 +17,31 @@ const userSocketMap = {}; // {userId->socketId}
 const getSocketId = (userId) => userSocketMap[userId];
 
 io.on("connection", (socket) => {
-    const userId = socket.handshake.query.userId;
-    if (userId !== undefined) {
-        userSocketMap[userId] = socket.id;
-    }
+	const userId = socket.handshake.query.userId;
+	if (userId !== undefined) {
+		// Handle multiple loggedin
+		if (userId in userSocketMap) {
+			userSocketMap[userId].push(socket.id);
+		} else {
+			userSocketMap[userId] = [socket.id];
+		}
+	}
 
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+	io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect", () => {
-        delete userSocketMap[userId];
-        io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    });
+	socket.on("disconnect", () => {
+		// Don't delete all soceketId
+		if (userId in userSocketMap) {
+			userSocketMap[userId] = userSocketMap[userId].filter(
+				(id) => id !== socket.id
+			);
+			if (userSocketMap[userId].length === 0) {
+				delete userSocketMap[userId];
+			}
+		}
+
+		io.emit("getOnlineUsers", Object.keys(userSocketMap));
+	});
 });
 
 module.exports = { app, io, server, getSocketId };
