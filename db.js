d374fc2b0a1f55e9a0fa8b0965266f15f9b68011
Garaisourav12 <garaisourav12@gmail.com>
2024-11-01 20:42:06 +0530

    Cluster for running atmost 2 instances

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("Connected to MongoDb");
-})
-.catch((err) => {
-    console.log(err.message);
-})
+// Connect to MongoDB
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.log(error.message);
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,39 +1,76 @@
 const express = require("express");
+const os = require("os");
+const cluster = require("cluster");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 // File Imports
 const { app, server } = require("./socket");
+const connectDB = require("./db");
 const routes = require("./routes");
 
 // Constants
 const PORT = process.env.PORT || 8000;
 const corsOptions = {
-    origin: true,
-    credentials: true,
+	origin: true,
+	credentials: true,
 };
+const numCPUs = os.cpus().length;
 
-// Middlewares
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors(corsOptions));
+// Start Server with Clustering
+(async () => {
+	if (cluster.isPrimary) {
+		console.log(`Master process ${process.pid} is running`);
 
-// Database Connection
-require("./db");
+		for (let i = 0; i < Math.min(numCPUs, 2); i++) {
+			cluster.fork();
+		}
 
-// Routes
-app.get("/", (req, res) => {
-    return res.json({
-        message: "Welcome to the TextMe API",
-        version: "1.0.0",
-    });
-});
-app.use("/api", routes);
+		cluster.on("exit", (worker, code, signal) => {
+			console.log(
+				`Worker process ${worker.process.pid} died. Restarting...`
+			);
+			cluster.fork();
+		});
+	} else {
+		// Connect to MongoDB
+		await connectDB();
 
-// Server Start
-server.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
-});
+		// Middlewares
+		app.use(express.json());
+		app.use(cookieParser());
+		app.use(cors(corsOptions));
+
+		app.use((req, res, next) => {
+			res.set("X-Process-ID", process.pid); // Set the process ID in the headers
+			next();
+		});
+
+		// Routes
+		app.get("/", (req, res) => {
+			// Entry point
+			return res.json({
+				message: "Welcome to the TextMe API",
+				version: "1.0.0",
+			});
+		});
+		app.use("/api", routes); // Functional routes
+		app.use((req, res) => {
+			// Unknown routes
+			return res.status(404).json({
+				message: `Cannot ${req.method} ${req.url}`,
+				error: "Not Found",
+				version: "1.0.0",
+			});
+		});
+
+		// Start Server
+		server.listen(PORT, () => {
+			console.log(
+				`Worker process ${process.pid} is listening on port ${PORT}`
+			);
+			console.log(`http://localhost:${PORT}`);
+		});
+	}
+})();
