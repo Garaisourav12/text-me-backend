3ed43165319fe93e3d7bd3c00327d7c2e36b4ab7
Garaisourav12 <garaisourav12@gmail.com>
2024-11-02 08:35:57 +0530

    Using same redis instance for all purpose

--- pubsub.js
+++ pubsub.js
@@ -1,17 +1,8 @@
-const Redis = require("ioredis");
+const redis = require("./redis");
 
-// Replace with your actual Redis credentials
-const publisher = new Redis({
-	host: process.env.REDIS_HOST, // e.g., 'localhost' or 'redis-12345.example.com'
-	port: process.env.REDIS_PORT, // Default Redis port
-	password: process.env.REDIS_PASS, // If a password is set
-});
+const publisher = redis.duplicate(); // Create a new client to publish messages
 
-const subscriber = new Redis({
-	host: process.env.REDIS_HOST, // e.g., 'localhost' or 'redis-12345.example.com'
-	port: process.env.REDIS_PORT, // Default Redis port
-	password: process.env.REDIS_PASS, // If a password is set
-});
+const subscriber = redis.duplicate(); // Create a new client to subscribe to messages
 
 subscriber.subscribe("login", "logout", "new-message");
 
